e92d3dbee9dcae327d5025f6039fb7517df083bf
stevenlustig <steven@androidsonskateboards.com>
2015-10-07 01:45:48 +0300

    Updated to library version 3.1.1

--- UmanoSlidingUpPanelAndroid/Properties/AssemblyInfo.cs
+++ UmanoSlidingUpPanelAndroid/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using Android.App;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -10,7 +8,7 @@ using Android.App;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Jay")]
+[assembly: AssemblyCopyright ("")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -18,7 +16,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.0")]
+[assembly: AssemblyVersion ("3.1.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
